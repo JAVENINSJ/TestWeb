183c3978362ce66bfac7832289b27a434a4cab6d
Mr. Nobody <Mr. Nobody@DESKTOP-19LLG4O>
2021-02-12 19:57:53 +0200

    v1.1 added next task

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+window.onscroll =()=>{scrollFun();};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function scrollFun(){
+    if(window.pageYOffset >= sticky){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    }
+}
+
+
